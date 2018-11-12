bb4dc92d7a091eb939fff3c24becb61a9da19068
Ivan Kolobaev <ikolobai@yandex.ru>
2018-11-12 08:58:52 +0300

    Удален дефолтный компонент, добавлен сомпонент Page

---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './Page.scss';
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="page__header">
+
+        </div>
+        <div className="page__content">
+
+        </div>
+        <div className="page__footer">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Page;
